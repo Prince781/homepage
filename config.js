85007aac9144ccc6a6e5ff0441fa73d154e1f919
Prince781 <princetonf@optonline.net>
2014-05-18 11:04:41 -0400

    Adding fonts and other code.

---
+++ config.js
@@ -0,0 +1,12 @@
+// config.js
+// include this BEFORE home.js
+"use strict";
+
+window.homepage = [
+// format: name, color, url, (styles: {})
+	["/a/", "#d9f0e2", "https://4chan.org/a/"],
+	["/g/", "#f4d8df", "https://4chan.org/g/"],
+	["mangapark", "#d9c9a5", "https://mangapark.com/", {small: true}],
+	["fb", "#d9e2f0", "https://facebook.com/"],
+	["yt", "#d9b9c2", "https://youtube.com/"]
+];
---
+++ home.js
@@ -0,0 +1,24 @@
+// home.js
+
+function newHPElement(title, color, url) {
+	var elem = document.createElement("div");
+	elem.className = "item";
+	elem.style.backgroundColor = color;
+	elem.setAttribute("href", url);
+	elem.innerHTML = title;
+	return elem;
+};
+
+window.onload = function() {
+	var centerbox = document.getElementById("centerbox");
+	for (var i=0; i<homepage.length; i++) {
+		var elem = newHPElement(homepage[i][0], homepage[i][1],
+					homepage[i][2]);
+		if (homepage[i].length == 4
+		&& typeof homepage[i][3] == "object") {
+			for (var name in homepage[i][3])
+				elem.setAttribute(name, homepage[i][3][name]);
+		}
+		centerbox.appendChild(elem);
+	}
+};
